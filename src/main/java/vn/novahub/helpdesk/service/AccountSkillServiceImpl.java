361f924782153f893d516ad0a92a2428b7667085
Quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2018-07-23 00:58:16 +0700

    [bugfix/HD-49] fix get all skills

--- src/main/java/vn/novahub/helpdesk/service/AccountSkillServiceImpl.java
+++ src/main/java/vn/novahub/helpdesk/service/AccountSkillServiceImpl.java
@@ -63,7 +63,7 @@ public class AccountSkillServiceImpl implements AccountSkillService {
 
     @Override
     public Page<Skill> getAllByKeyword(String keyword, Pageable pageable) {
-        return skillRepository.getAllByNameLike("%" + keyword + "&", pageable);
+        return skillRepository.getAllByNameLike("%" + keyword + "%", pageable);
     }
 
 
