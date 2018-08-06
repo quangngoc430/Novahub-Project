973b2ccbd497544110ae77da401dc41ac511075f
Bui Lam Quang Ngoc <ngocbui@novahub.vn>
2018-08-06 08:36:01 +0000

    [feature/HD-40] SeedingSkill.java edited online with Bitbucket

--- src/main/java/vn/novahub/helpdesk/seeding/SeedingSkill.java
+++ src/main/java/vn/novahub/helpdesk/seeding/SeedingSkill.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 @Component
-public class SkillSeeding {
+public class SeedingSkill {
 
     @Autowired
     private SkillRepository skillRepository;
