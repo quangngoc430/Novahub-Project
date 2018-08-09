e2fa973facdfb55f43578d2cbbea42ed68bdb45a
Bui Lam Quang Ngoc <ngocbui@novahub.vn>
2018-08-09 03:10:23 +0000

    [feature/HD-40] change name of class SeedingAccount to AccountsSeeder

--- src/main/java/vn/novahub/helpdesk/seeding/SeedingAccount.java
+++ src/main/java/vn/novahub/helpdesk/seeding/SeedingAccount.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 @Component
-public class SeedingAccount {
+public class AccountsSeeder {
 
     @Autowired
     private ResourceLoader resourceLoader;
