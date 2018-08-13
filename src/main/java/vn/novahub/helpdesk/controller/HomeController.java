913a593a4a4ea9f05fe9496237a2a2e69989b997
Bui Lam Quang Ngoc <ngocbui@novahub.vn>
2018-08-13 08:44:26 +0000

    [feature/HD-94] edit code into class HomeController

--- src/main/java/vn/novahub/helpdesk/controller/HomeController.java
+++ src/main/java/vn/novahub/helpdesk/controller/HomeController.java
@@ -6,27 +6,6 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
-<<<<<<< HEAD
-=======
-    private static String PREFIX = "ROLE_";
-    
-    @PermitAll
-    @RequestMapping("/login")
-    public String login() {
-
-        String roleName = (SecurityContextHolder.getContext().getAuthentication().getAuthorities().toArray())[0].toString();
-
-        if(roleName.equals(PREFIX + RoleEnum.ADMIN.name()))
-            return "redirect:/admin";
-        else if(roleName.equals(PREFIX + RoleEnum.CLERK.name()))
-            return "redirect:/clerk";
-        else if(roleName.equals(PREFIX + RoleEnum.EMPLOYEE.name()))
-            return "redirect:/user";
-
-        return "login";
-    }
-
->>>>>>> origin/develop
     @RequestMapping("/swagger/openapi.json")
     public String swagger() {
         return "../swagger/example/openapi.json";
