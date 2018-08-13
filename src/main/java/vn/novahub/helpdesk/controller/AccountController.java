7ceed58cdc910b02abe3d8237c51c89ea5e9e9f5
Bui Lam Quang Ngoc <ngocbui@novahub.vn>
2018-08-13 10:25:14 +0000

    [feature/HD-94] import class model Skill;

--- src/main/java/vn/novahub/helpdesk/controller/AccountController.java
+++ src/main/java/vn/novahub/helpdesk/controller/AccountController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import vn.novahub.helpdesk.exception.*;
 import vn.novahub.helpdesk.model.Account;
 import vn.novahub.helpdesk.model.Token;
+import vn.novahub.helpdesk.model.Skill;
 import vn.novahub.helpdesk.service.AccountService;
 import vn.novahub.helpdesk.service.AccountSkillService;
 
