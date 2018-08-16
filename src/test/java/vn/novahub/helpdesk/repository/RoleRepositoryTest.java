e0fb9ceabd6213c4473940e2edf32edbc4743e10
Quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2018-08-16 15:32:04 +0700

    [feature/HD-48] add some tests into class RoleRepositoryTest

--- src/test/java/vn/novahub/helpdesk/repository/RoleRepositoryTest.java
+++ src/test/java/vn/novahub/helpdesk/repository/RoleRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import vn.novahub.helpdesk.model.Role;
 
 public class RoleRepositoryTest extends BaseRepositoryTest {
@@ -26,18 +28,46 @@ public class RoleRepositoryTest extends BaseRepositoryTest {
     @Test
     public void testGetARole() {
         Role role = new Role();
-        role.setId(1l);
         role.setName("test");
-        repository.save(role);
+        role = repository.save(role);
 
-        Role r = repository.getById(1l);
-        Assertions.assertThat(r.getId()).isEqualTo(1l);
+        Role r = repository.getById(role.getId());
+        Assertions.assertThat(r.getId()).isEqualTo(role.getId());
         Assertions.assertThat(r.getName()).isEqualTo("test");
     }
 
     @Test
+    public void testGetARoleByName() {
+        Role role = new Role();
+        role.setName("ADMIN");
+        repository.save(role);
+
+        Role r = repository.getByName("ADMIN");
+        Assertions.assertThat(r.getName()).isEqualTo("ADMIN");
+    }
+
+    @Test
+    public void testGetAllByNameContaining() {
+        Role role = new Role();
+        role.setName("ADMIN");
+        repository.save(role);
+
+        role = new Role();
+        role.setName("CLERK");
+        repository.save(role);
+
+        role = new Role();
+        role.setName("EMPLOYEE");
+        repository.save(role);
+
+        Page<Role> roles = repository.getAllByNameContaining("AD", PageRequest.of(0, 20));
+        Assertions.assertThat(roles.getContent().get(0).getName()).isEqualTo("ADMIN");
+        Assertions.assertThat(roles.getContent().size()).isEqualTo(1);
+    }
+
+    @Test
     public void testGetRoleNotFound() {
-        Role role = repository.getById(1000l);
+        Role role = repository.getById(1000L);
         Assert.assertNull(role);
     }
 }
