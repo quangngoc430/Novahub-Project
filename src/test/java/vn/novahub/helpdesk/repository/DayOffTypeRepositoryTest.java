7aea7be435ab17b32979f2a96865f8f1d4cfbce1
tranmanhlinh <linhtran@novahub.vn>
2018-08-20 17:57:47 +0700

    [testing/HD-45] DayOffType testing

---
+++ src/test/java/vn/novahub/helpdesk/repository/DayOffTypeRepositoryTest.java
@@ -0,0 +1,31 @@
+package vn.novahub.helpdesk.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import vn.novahub.helpdesk.model.DayOffType;
+
+import static org.junit.Assert.*;
+
+public class DayOffTypeRepositoryTest {
+
+    @Autowired
+    private DayOffTypeRepository dayOffTypeRepository;
+
+    private DayOffType dayOffType;
+
+    @Before
+    public void before() {
+        dayOffTypeRepository.deleteAll();
+        dayOffType = new DayOffType();
+        dayOffType.setType("Pregnant");
+        dayOffType.setDefaultQuota(24);
+    }
+
+    @Test
+    public void testSaveAndFindByType() throws Exception {
+        dayOffTypeRepository.save(dayOffType);
+        assertEquals(dayOffType, dayOffTypeRepository.findByType("Pregnant"));
+    }
+
+}
