2ebe8bda5f6012d336ccf81b616ecacf219249ff
tranmanhlinh <linhtran@novahub.vn>
2018-08-28 15:12:25 +0700

    [testing/HD-46] Done test DayOffAccountAdmin Controller

--- src/main/java/vn/novahub/helpdesk/Application.java
+++ src/main/java/vn/novahub/helpdesk/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import vn.novahub.helpdesk.seeding.ApplicationSeeder;
 
@@ -16,6 +17,7 @@ import java.text.ParseException;
 
 @Configuration
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableAsync
 public class Application {
 
--- src/test/java/vn/novahub/helpdesk/controller/DayOffAccountAdminControllerTest.java
+++ src/test/java/vn/novahub/helpdesk/controller/DayOffAccountAdminControllerTest.java
@@ -1,6 +1,7 @@
 package vn.novahub.helpdesk.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,13 +19,17 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(DayOffAccountController.class)
+@WebMvcTest(DayOffAccountAdminController.class)
 public class DayOffAccountAdminControllerTest extends BaseControllerTest {
 
     @MockBean
@@ -32,6 +37,8 @@ public class DayOffAccountAdminControllerTest extends BaseControllerTest {
 
     @MockBean
     private AccountService accountService;
+
+    private ObjectMapper objectMapper;
 
     private List<DayOffAccount> dayOffAccounts;
 
@@ -46,23 +53,66 @@ public class DayOffAccountAdminControllerTest extends BaseControllerTest {
                 "seeding/accounts.json",
                 new TypeReference<List<Account>>(){});
         given(accountService.getAccountLogin()).willReturn(accounts.get(0));
-
+        objectMapper = new ObjectMapper();
     }
 
     @Test
     public void testCreate() throws Exception {
+        given(dayOffAccountService.add(any(DayOffAccount.class))).willReturn(dayOffAccounts.get(0));
 
-        dayOffAccounts.remove(2);
-        given(dayOffAccountService.findByAccountId(1, PageRequest.of(0, 20)))
+        mvc.perform(post("/api/admin/day-off-accounts")
+                .with(user(EMAIL).password(PASSWORD))
+                .with(csrf().asHeader())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dayOffAccounts.get(0))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.privateQuota", is(dayOffAccounts.get(0).getPrivateQuota())));
+    }
+
+    @Test
+    public void testGet_noParam() throws Exception {
+        given(dayOffAccountService.getAll(PageRequest.of(0, 20)))
                 .willReturn(new PageImpl<>(dayOffAccounts));
 
-        mvc.perform(get("/api/day-off-accounts")
+        mvc.perform(get("/api/admin/day-off-accounts")
                 .with(user(EMAIL).password(PASSWORD))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content.length()", is(2)))
-                .andExpect(jsonPath("$.content[0].privateQuota", is(88)));
+                .andExpect(jsonPath("$.content.length()", is(dayOffAccounts.size())))
+                .andExpect(jsonPath("$.content[1].remainingTime", is(dayOffAccounts.get(1).getRemainingTime())));
     }
+
+    @Test
+    public void testGet_withParameterOfAccountId() throws Exception {
+
+        dayOffAccounts.remove(2);
+
+        given(dayOffAccountService.findByAccountId(1, PageRequest.of(0, 20)))
+                .willReturn(new PageImpl<>(dayOffAccounts));
+
+        mvc.perform(get("/api/admin/day-off-accounts")
+                .with(user(EMAIL).password(PASSWORD))
+                .param("accountId", "1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.length()", is(dayOffAccounts.size())));
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+        given(dayOffAccountService.update(any(DayOffAccount.class))).willReturn(dayOffAccounts.get(0));
+
+        mvc.perform(put("/api/admin/day-off-accounts/1")
+                .with(user(EMAIL).password(PASSWORD))
+                .with(csrf().asHeader())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dayOffAccounts.get(0))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.privateQuota", is(dayOffAccounts.get(0).getPrivateQuota())));
+    }
+
+
 
 
 }
