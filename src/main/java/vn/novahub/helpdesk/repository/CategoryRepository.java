0951eac6d010a4bf52bcf88a50e93d6fd9d65ef3
Quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2018-09-04 01:55:49 +0700

    [feature/HD-47] done test CategoryRepositoryTest class

--- src/main/java/vn/novahub/helpdesk/repository/CategoryRepository.java
+++ src/main/java/vn/novahub/helpdesk/repository/CategoryRepository.java
@@ -3,19 +3,16 @@ package vn.novahub.helpdesk.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.novahub.helpdesk.model.Category;
-
-import java.util.List;
 
 @Repository
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
 
-    Page<Category> getAllByNameContaining(String name, Pageable pageable);
+    Page<Category> getAllByNameContaining(@Param("name") String name, Pageable pageable);
 
     boolean existsByName(String name);
 
     boolean existsById(long categoryId);
-
-    Category getByName(String name);
 }
