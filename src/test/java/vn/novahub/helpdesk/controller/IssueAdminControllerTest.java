0b33158a12d06a5d90d09b013b73b98752f5521f
Quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2018-09-05 01:26:07 +0700

    [Testing/HD-44] add full code for testing class IssueAdminControllerTest

--- src/test/java/vn/novahub/helpdesk/controller/IssueAdminControllerTest.java
+++ src/test/java/vn/novahub/helpdesk/controller/IssueAdminControllerTest.java
@@ -9,9 +9,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
-import vn.novahub.helpdesk.model.Account;
+import vn.novahub.helpdesk.enums.IssueEnum;
 import vn.novahub.helpdesk.model.Issue;
-import vn.novahub.helpdesk.service.AccountIssueService;
+import vn.novahub.helpdesk.service.AdminIssueService;
 
 import java.io.IOException;
 import java.util.List;
@@ -26,24 +26,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(IssueController.class)
-public class IssueControllerTest extends BaseControllerTest {
+@WebMvcTest(IssueAdminController.class)
+public class IssueAdminControllerTest extends BaseControllerTest {
 
     @MockBean
-    private AccountIssueService accountIssueService;
+    private AdminIssueService adminIssueService;
 
     private ObjectMapper objectMapper;
-
-    private List<Account> accounts;
 
     private List<Issue> issues;
 
     @Before
     public void before() throws IOException {
-        accounts = convertJsonFileToObjectList(
-                "seeding/accounts.json",
-                new TypeReference<List<Account>>(){});
-
         issues = convertJsonFileToObjectList(
                 "seeding/issues.json",
                 new TypeReference<List<Issue>>(){});
@@ -52,69 +46,15 @@ public class IssueControllerTest extends BaseControllerTest {
     }
 
     @Test
-    public void testGetAll() throws Exception {
+    public void testGetAllByAdmin() throws Exception {
         for (int i = issues.size() - 1; i >= 0; i--) {
-            if (issues.get(i).getAccountId() != 1)
+            if (!issues.get(i).getStatus().equals(IssueEnum.PENDING.name()))
                 issues.remove(i);
         }
 
-        given(accountIssueService.getAllByKeywordAndStatus("", "", new PageRequest(0, 20))).willReturn(new PageImpl<>(issues));
+        given(adminIssueService.getAllByKeywordAndStatus("", "PENDING", new PageRequest(0, 20))).willReturn(new PageImpl<>(issues));
 
-        mvc.perform(get("/api/issues/me")
-                    .with(user(EMAIL).password(PASSWORD))
-                    .with(csrf().asHeader())
-                    .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.totalElements", is(issues.size())));
-    }
-
-    @Test
-    public void testCreate() throws Exception {
-        Issue issue = issues.get(0);
-
-        given(accountIssueService.create(any(Issue.class))).willReturn(issue);
-
-        mvc.perform(post("/api/issues/me")
-                    .with(user(EMAIL).password(PASSWORD))
-                    .with(csrf().asHeader())
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(issue)))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.title", is(issues.get(0).getTitle())));
-    }
-
-    @Test
-    public void testFindOne() throws Exception {
-        given(accountIssueService.findOne(1L)).willReturn(issues.get(0));
-
-        mvc.perform(get("/api/issues/me/1")
-                    .with(user(EMAIL).password(PASSWORD))
-                    .with(csrf().asHeader())
-                    .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.title", is(issues.get(0).getTitle())));
-    }
-
-    @Test
-    public void testUpdate() throws Exception {
-        Issue issue = issues.get(0);
-        issue.setTitle("ABC");
-
-        given(accountIssueService.update(anyLong(), any(Issue.class))).willReturn(issue);
-
-        mvc.perform(put("/api/issues/me/1")
-                    .with(user(EMAIL).password(PASSWORD))
-                    .with(csrf().asHeader())
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(issue)))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.title", is("ABC")));
-    }
-
-    @Test
-    public void testDelete() throws Exception {
-
-        mvc.perform(delete("/api/issues/me/1")
+        mvc.perform(get("/api/admin/issues")
                     .with(user(EMAIL).password(PASSWORD))
                     .with(csrf().asHeader())
                     .contentType(MediaType.APPLICATION_JSON))
@@ -122,8 +62,45 @@ public class IssueControllerTest extends BaseControllerTest {
     }
 
     @Test
-    public void testAction() throws Exception {
-        mvc.perform(get("/api/issues/1/action?status=APPROVE&token=123456789")
+    public void testFindOneByAdmin() throws Exception {
+        given(adminIssueService.findOne(1)).willReturn(issues.get(0));
+
+        mvc.perform(get("/api/admin/issues/1")
+                    .with(user(EMAIL).password(PASSWORD))
+                    .with(csrf())
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.title", is(issues.get(0).getTitle())));
+    }
+
+    @Test
+    public void testUpdateForAmin() throws Exception {
+        Issue issue = issues.get(0);
+        issue.setTitle("1234");
+
+        given(adminIssueService.update(anyLong(), any(Issue.class))).willReturn(issue);
+
+        mvc.perform(put("/api/admin/issues/1")
+                    .with(user(EMAIL).password(PASSWORD))
+                    .with(csrf().asHeader())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(issue)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.title", is("1234")));
+    }
+
+    @Test
+    public void testDeleteByAdmin() throws Exception {
+        mvc.perform(delete("/api/admin/issues/1")
+                    .with(user(EMAIL).password(PASSWORD))
+                    .with(csrf().asHeader())
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testActionForAdmin() throws Exception {
+        mvc.perform(put("/api/admin/issues/1/action?status=APPROVE")
                     .with(user(EMAIL).password(PASSWORD))
                     .with(csrf().asHeader())
                     .contentType(MediaType.APPLICATION_JSON))
