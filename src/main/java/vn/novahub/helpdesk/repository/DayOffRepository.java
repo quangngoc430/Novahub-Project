2133db6142cc06280ce3426e62b96e3d5738308a
tranmanhlinh <linhtran@novahub.vn>
2018-09-05 13:25:53 +0700

    [feature/HD-139] Remove commented code in DayOffRepository

--- src/main/java/vn/novahub/helpdesk/repository/DayOffRepository.java
+++ src/main/java/vn/novahub/helpdesk/repository/DayOffRepository.java
@@ -45,31 +45,4 @@ public interface DayOffRepository extends PagingAndSortingRepository<DayOff, Lon
             "WHERE dayOff.status <> 'PENDING'")
     Page<DayOff> findAllAnswered(Pageable pageable);
 
-//    @Query("SELECT dayOff " +
-//            "FROM DayOff dayOff " +
-//            "JOIN DayOffAccount dayOffAccount " +
-//            "ON dayOff.dayOffAccountId = dayOffAccount.id " +
-//            "JOIN DayOffType dayOffType " +
-//            "ON dayOffAccount.dayOffTypeId = dayOffType.id " +
-//            "JOIN Account account " +
-//            "ON dayOffAccount.accountId = account.id " +
-//            "WHERE lower(dayOffType.type) LIKE lower(CONCAT('%', :keyword ,'%')) " +
-//            "OR lower(account.email) LIKE lower(CONCAT('%', :keyword, '%'))")
-//    Page<DayOff> findByKeyword(@Param("keyword") String keyword,
-//                                                Pageable pageable);
-//
-//
-//    @Query("SELECT dayOff " +
-//            "FROM DayOff dayOff " +
-//            "JOIN DayOffAccount dayOffAccount " +
-//            "ON dayOff.dayOffAccountId = dayOffAccount.id " +
-//            "JOIN DayOffType dayOffType " +
-//            "ON dayOffAccount.dayOffTypeId = dayOffType.id " +
-//            "JOIN Account account " +
-//            "ON dayOffAccount.accountId = account.id " +
-//            "WHERE (lower(dayOffType.type) LIKE lower(CONCAT('%', :keyword, '%')) " +
-//            "OR lower(account.email) LIKE lower(CONCAT('%', :keyword, '%'))) " +
-//            "AND dayOff.status <> 'CANCELLED'")
-//    Page<DayOff> findNonCancelledByKeyword( @Param("keyword") String keyword,
-//                                Pageable pageable);
 }
